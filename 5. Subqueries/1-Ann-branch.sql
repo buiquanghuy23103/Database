872921a22352f8af5340ea27717947e9d609dec3
Huy Bui <huythdnbkltv@gmail.com>
2018-02-14 10:13:53 +0200

    Update 1-Ann-branch.sql

--- 5. Subqueries/1-Ann-branch.sql
+++ 5. Subqueries/1-Ann-branch.sql
@@ -1,4 +1,4 @@
 SELECT Staff.FirstName, Staff.FamilyName, Branch.City, Branch.Street
 FROM Branch
-FULL OUTER JOIN Staff ON Branch.Id=Staff.BranchId
+JOIN Staff ON Branch.Id=Staff.BranchId
 WHERE Staff.FirstName='Ann'
