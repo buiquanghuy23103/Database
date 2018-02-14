2578d3fa646aef4483ec3b7eef84850c051b2e74
Huy Bui <huythdnbkltv@gmail.com>
2018-02-14 10:46:44 +0200

    subquery version

--- 5. Subqueries/6-null-comment.sql
+++ 5. Subqueries/6-null-comment.sql
@@ -1,4 +1,11 @@
 SELECT DISTINCT Client.FirstName,Client.FamilyName,Viewing.ViewDate,Viewing.CommentsGiven
 FROM Client
-FULL OUTER JOIN Viewing ON Client.Id=Viewing.ClientId
+JOIN Viewing ON Client.Id=Viewing.ClientId
 WHERE Viewing.ViewDate='2018-06-15' AND Viewing.CommentsGiven IS NULL
+
+SELECT DISTINCT FirstName,FamilyName
+FROM Client
+WHERE Id IN
+  (SELECT ClientId
+  FROM Viewing
+  WHERE ViewDate='2018-06-15' AND CommentsGiven IS NULL)
